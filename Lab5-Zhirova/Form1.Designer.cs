c8ba733561bca4ffa0176eb0a29f87a7f5eecb75
Angela138 <zhirova.anzhela@gmail.com>
2023-01-14 14:20:03 +0300

    Add new files

---
+++ Lab5-Zhirova/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace Lab5_Zhirova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bInFile = new System.Windows.Forms.Button();
+            this.bOutFile = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.bClean = new System.Windows.Forms.Button();
+            this.rB_CRC32 = new System.Windows.Forms.RadioButton();
+            this.rB_HAVAL = new System.Windows.Forms.RadioButton();
+            this.rB_RIPEMD160 = new System.Windows.Forms.RadioButton();
+            this.rB_MD5 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA1 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA256 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA384 = new System.Windows.Forms.RadioButton();
+            this.rB_SHA512 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelOutLength = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tInFilePath = new System.Windows.Forms.TextBox();
+            this.tOutFilePath = new System.Windows.Forms.TextBox();
+            this.label_time = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelInLength = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bInFile
+            // 
+            this.bInFile.Location = new System.Drawing.Point(366, 24);
+            this.bInFile.Name = "bInFile";
+            this.bInFile.Size = new System.Drawing.Size(41, 23);
+            this.bInFile.TabIndex = 0;
+            this.bInFile.Text = "...";
+            this.bInFile.UseVisualStyleBackColor = true;
+            this.bInFile.Click += new System.EventHandler(this.bInFile_Click);
+            // 
+            // bOutFile
+            // 
+            this.bOutFile.Location = new System.Drawing.Point(366, 53);
+            this.bOutFile.Name = "bOutFile";
+            this.bOutFile.Size = new System.Drawing.Size(41, 23);
+            this.bOutFile.TabIndex = 1;
+            this.bOutFile.Text = "...";
+            this.bOutFile.UseVisualStyleBackColor = true;
+            this.bOutFile.Click += new System.EventHandler(this.bOutFile_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(249, 236);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(64, 32);
+            this.bStart.TabIndex = 2;
+            this.bStart.Text = "Виконати";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bClean
+            // 
+            this.bClean.Location = new System.Drawing.Point(350, 236);
+            this.bClean.Name = "bClean";
+            this.bClean.Size = new System.Drawing.Size(70, 32);
+            this.bClean.TabIndex = 3;
+            this.bClean.Text = "Очистити";
+            this.bClean.UseVisualStyleBackColor = true;
+            this.bClean.Click += new System.EventHandler(this.bClean_Click);
+            // 
+            // rB_CRC32
+            // 
+            this.rB_CRC32.AutoSize = true;
+            this.rB_CRC32.Location = new System.Drawing.Point(14, 19);
+            this.rB_CRC32.Name = "rB_CRC32";
+            this.rB_CRC32.Size = new System.Drawing.Size(59, 17);
+            this.rB_CRC32.TabIndex = 4;
+            this.rB_CRC32.TabStop = true;
+            this.rB_CRC32.Text = "CRC32";
+            this.rB_CRC32.UseVisualStyleBackColor = true;
+            // 
+            // rB_HAVAL
+            // 
+            this.rB_HAVAL.AutoSize = true;
+            this.rB_HAVAL.Location = new System.Drawing.Point(14, 43);
+            this.rB_HAVAL.Name = "rB_HAVAL";
+            this.rB_HAVAL.Size = new System.Drawing.Size(94, 17);
+            this.rB_HAVAL.TabIndex = 5;
+            this.rB_HAVAL.TabStop = true;
+            this.rB_HAVAL.Text = "HashAlgonthm";
+            this.rB_HAVAL.UseVisualStyleBackColor = true;
+            // 
+            // rB_RIPEMD160
+            // 
+            this.rB_RIPEMD160.AutoSize = true;
+            this.rB_RIPEMD160.Location = new System.Drawing.Point(14, 67);
+            this.rB_RIPEMD160.Name = "rB_RIPEMD160";
+            this.rB_RIPEMD160.Size = new System.Drawing.Size(85, 17);
+            this.rB_RIPEMD160.TabIndex = 6;
+            this.rB_RIPEMD160.TabStop = true;
+            this.rB_RIPEMD160.Text = "RIPEMD160";
+            this.rB_RIPEMD160.UseVisualStyleBackColor = true;
+            // 
+            // rB_MD5
+            // 
+            this.rB_MD5.AutoSize = true;
+            this.rB_MD5.Location = new System.Drawing.Point(14, 91);
+            this.rB_MD5.Name = "rB_MD5";
+            this.rB_MD5.Size = new System.Drawing.Size(48, 17);
+            this.rB_MD5.TabIndex = 7;
+            this.rB_MD5.TabStop = true;
+            this.rB_MD5.Text = "MD5";
+            this.rB_MD5.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA1
+            // 
+            this.rB_SHA1.AutoSize = true;
+            this.rB_SHA1.Location = new System.Drawing.Point(164, 19);
+            this.rB_SHA1.Name = "rB_SHA1";
+            this.rB_SHA1.Size = new System.Drawing.Size(56, 17);
+            this.rB_SHA1.TabIndex = 8;
+            this.rB_SHA1.TabStop = true;
+            this.rB_SHA1.Text = "SHA-1";
+            this.rB_SHA1.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA256
+            // 
+            this.rB_SHA256.AutoSize = true;
+            this.rB_SHA256.Location = new System.Drawing.Point(164, 43);
+            this.rB_SHA256.Name = "rB_SHA256";
+            this.rB_SHA256.Size = new System.Drawing.Size(68, 17);
+            this.rB_SHA256.TabIndex = 9;
+            this.rB_SHA256.TabStop = true;
+            this.rB_SHA256.Text = "SHA-256";
+            this.rB_SHA256.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA384
+            // 
+            this.rB_SHA384.AutoSize = true;
+            this.rB_SHA384.Location = new System.Drawing.Point(164, 67);
+            this.rB_SHA384.Name = "rB_SHA384";
+            this.rB_SHA384.Size = new System.Drawing.Size(68, 17);
+            this.rB_SHA384.TabIndex = 10;
+            this.rB_SHA384.TabStop = true;
+            this.rB_SHA384.Text = "SHA-384";
+            this.rB_SHA384.UseVisualStyleBackColor = true;
+            // 
+            // rB_SHA512
+            // 
+            this.rB_SHA512.AutoSize = true;
+            this.rB_SHA512.Location = new System.Drawing.Point(164, 91);
+            this.rB_SHA512.Name = "rB_SHA512";
+            this.rB_SHA512.Size = new System.Drawing.Size(68, 17);
+            this.rB_SHA512.TabIndex = 11;
+            this.rB_SHA512.TabStop = true;
+            this.rB_SHA512.Text = "SHA-512";
+            this.rB_SHA512.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rB_SHA256);
+            this.groupBox1.Controls.Add(this.rB_SHA512);
+            this.groupBox1.Controls.Add(this.rB_MD5);
+            this.groupBox1.Controls.Add(this.rB_SHA1);
+            this.groupBox1.Controls.Add(this.rB_CRC32);
+            this.groupBox1.Controls.Add(this.rB_RIPEMD160);
+            this.groupBox1.Controls.Add(this.rB_HAVAL);
+            this.groupBox1.Controls.Add(this.rB_SHA384);
+            this.groupBox1.Location = new System.Drawing.Point(12, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 119);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим роботи:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelInLength);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(295, 84);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 40);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Оцінка розміру (Мбайт):";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Вхідного файлу:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelOutLength);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(295, 141);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 51);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Оцінка розміру (Байти):";
+            // 
+            // labelOutLength
+            // 
+            this.labelOutLength.AutoSize = true;
+            this.labelOutLength.Location = new System.Drawing.Point(102, 27);
+            this.labelOutLength.Name = "labelOutLength";
+            this.labelOutLength.Size = new System.Drawing.Size(10, 13);
+            this.labelOutLength.TabIndex = 1;
+            this.labelOutLength.Text = " ";
+            this.labelOutLength.Click += new System.EventHandler(this.labelOutLength_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Вихідного файлу:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Вхідний файл:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Вихідний файл:";
+            // 
+            // tInFilePath
+            // 
+            this.tInFilePath.Location = new System.Drawing.Point(120, 24);
+            this.tInFilePath.Name = "tInFilePath";
+            this.tInFilePath.Size = new System.Drawing.Size(229, 20);
+            this.tInFilePath.TabIndex = 17;
+            // 
+            // tOutFilePath
+            // 
+            this.tOutFilePath.Location = new System.Drawing.Point(120, 52);
+            this.tOutFilePath.Name = "tOutFilePath";
+            this.tOutFilePath.Size = new System.Drawing.Size(229, 20);
+            this.tOutFilePath.TabIndex = 18;
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Location = new System.Drawing.Point(319, 208);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(10, 13);
+            this.label_time.TabIndex = 19;
+            this.label_time.Text = " ";
+            this.label_time.Click += new System.EventHandler(this.label_time_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(26, 236);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(37, 13);
+            this.linkLabel1.TabIndex = 20;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Автор";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(105, 236);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(33, 13);
+            this.linkLabel2.TabIndex = 21;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Опис";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(301, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Тривалість кодування:";
+            // 
+            // labelInLength
+            // 
+            this.labelInLength.AutoSize = true;
+            this.labelInLength.Location = new System.Drawing.Point(100, 21);
+            this.labelInLength.Name = "labelInLength";
+            this.labelInLength.Size = new System.Drawing.Size(13, 13);
+            this.labelInLength.TabIndex = 1;
+            this.labelInLength.Text = "  ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 280);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.tOutFilePath);
+            this.Controls.Add(this.tInFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bClean);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.bOutFile);
+            this.Controls.Add(this.bInFile);
+            this.Name = "Form1";
+            this.Text = "Циклічний надлишковий код (CRC)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bInFile;
+        private System.Windows.Forms.Button bOutFile;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bClean;
+        private System.Windows.Forms.RadioButton rB_CRC32;
+        private System.Windows.Forms.RadioButton rB_HAVAL;
+        private System.Windows.Forms.RadioButton rB_RIPEMD160;
+        private System.Windows.Forms.RadioButton rB_MD5;
+        private System.Windows.Forms.RadioButton rB_SHA1;
+        private System.Windows.Forms.RadioButton rB_SHA256;
+        private System.Windows.Forms.RadioButton rB_SHA384;
+        private System.Windows.Forms.RadioButton rB_SHA512;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelOutLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tInFilePath;
+        private System.Windows.Forms.TextBox tOutFilePath;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelInLength;
+    }
+}
+
